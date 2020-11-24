a3d9a6e878b4dd068bf498a83687cf68d6e2df1d
lvp1890 <leticiavp1890@gmail.com>
2020-11-24 17:15:40 -0300

    se concluye el trabajo

--- pda.py
+++ pda.py
@@ -1,35 +1,15 @@
 
 #RSV
-def Rsvq6(w,pila,PDA): #estado final 
+def Rsvq6(w,pila,PDA):  
     if (pila==""):
         PDA=False
     simbolo = w[0:1]
     w=w[1:]
-    pila=pila[1:] #consume un elemento de la pila
-    print("Rsvq6:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
+    pila=pila[1:] 
     return (w,pila,PDA)
-
-"""def Rsvq5(w,pila,PDA):
-    simbolo = w[0:1]
-    w=w[1:]
-    pila="$" + pila
-    print("Rsvq5:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
-    return (w,pila,PDA)
-
-def Rsvq4(w,pila,PDA): #estado final 
-    if (pila==""):
-        PDA=False
-    simbolo = w[0:1]
-    w=w[1:]
-    pila=pila[1:] #consume un elemento de la pila
-    print("Rsvq4:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
-    return (w,pila,PDA)"""
 
 def Rsvq5(w):
     simbolo = w[0:1]
-    
-    #pila=pila[1:]
-    print("Rsvq5:simbolo ->" + simbolo  +" | w ->" +w)
     w=w[1:]
     return (w)
 
@@ -37,14 +17,12 @@ def Rsvq4(w,pila,PDA):
     simbolo = w[0:1]
     w=w[1:]
     pila=pila[1:]
-    print("Rsvq4:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
     return (w,pila,PDA)
 
 def Rsvq3(w,pila,PDA):
     simbolo = w[0:1]
     w=w[1:]
     pila="&" + pila
-    print("Rsvq3:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
     return (w,pila,PDA)
     
 
@@ -53,16 +31,15 @@ def Rsvq2(w,pila,PDA): #estado final
         PDA=False
     simbolo = w[0:1]
     w=w[1:]
-    pila=pila[1:] #consume un elemento de la pila
-    print("Rsvq2:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
+    pila=pila[1:] 
     return (w,pila,PDA)
 
 def Rsvq1(w,pila,PDA):
     simbolo = w[0:1]
     w=w[1:]
     pila="$" + pila
-    print("Rsvq1:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
     return (w,pila,PDA)
+
 
 # -----------PDA RSV -------------#
 def pdaRsv(w, pila, PDA):
@@ -108,16 +85,14 @@ def pdaRsv(w, pila, PDA):
         
     while (len(w)>=1):
         if (w[0:1] =="A"):
-            #llamar rsv5
             w=Rsvq5(w)
-            #print("Rsvq5:simbolo ->A" + w[0:1] + " | w ->" +w)
-            #w=w[1:]
+            
             
         else:
             simbolo=w[0]
             w=w[1:]
             pila="&" + pila
-            print("Rsvq5:simbolo ->" + simbolo + " | w ->" +w)
+           
 
 
     if (not PDA):
@@ -133,7 +108,6 @@ def pdaRsv(w, pila, PDA):
 
 
 
-
 ########------ COVID --------########
 
 def q4(w,pila,PDA):
@@ -141,29 +115,25 @@ def q4(w,pila,PDA):
         PDA=False
     simbolo = w[0:1]
     w=w[1:]
-    pila=pila[1:] #consume un elemento de la pila
-    print("q4:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
+    pila=pila[1:] 
     return (w,pila,PDA)
 
 def q3(w,pila,PDA):
     simbolo = w[0:1]
     w=w[1:]
     pila=pila[1:]
-    print("q3:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
     return (w,pila,PDA)
 
 def q2(w,pila,PDA):
     simbolo = w[0:1]
     w=w[1:]
     pila="$" + pila
-    print("q2:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
     return (w,pila,PDA)
 
 def q1(w,pila):
-    simbolo=w[0:1] #saca el primer elemento 
-    w=w[1:] #todo menos el primer elemento 
+    simbolo=w[0:1] 
+    w=w[1:] 
     pila="$" + pila
-    print("q1:simbolo ->" + simbolo + "| w->" +w+ "pila ->" + pila)
     return(w,pila)
 
 ############ PDA (1): COVID ##########
@@ -181,54 +151,49 @@ def pdaCovid(w, pila, PDA):
         w,pila,PDA=q4(w,pila,PDA)
 
     if (not PDA):
-        print("pda: No reconocida [COVID-19]")
+        print("pda: No reconocida [COVID-19]", end=" ")
         return False
     else:
         if(len(w)==0 and len(pila)==0):
-            print("len: Reconocida [COVID-19]")
+            print("len: Reconocida [COVID-19]", end=" ")
             return True
         else:
-            print("len: NO reconocida [COVID-19]")
+            print("len: NO reconocida [COVID-19]", end=" ")
             return False
 
 # -------------Neumonia-------------#
-def Neuq7(w,pila,PDA): #estado final 
+def Neuq7(w,pila,PDA):  
     if (pila==""):
         PDA=False
     simbolo = w[0:1]
     w=w[1:]
-    pila=pila[1:] #consume un elemento de la pila
-    print("Neuq7:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
+    pila=pila[1:] 
     return (w,pila,PDA)
 
-def Neuq6(w,pila,PDA): #retiro 
+def Neuq6(w,pila,PDA): 
     simbolo = w[0:1]
     w=w[1:]
     pila=pila[1:]
-    print("Neuq6:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
     return (w,pila,PDA)
 
 def Neuq5(w,pila,PDA):
     simbolo = w[0:1]
     w=w[1:]
     pila="$$" + pila
-    print("Neuq1:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
     return (w,pila,PDA)
 
-def Neuq4(w,pila,PDA): #retiro 
+def Neuq4(w,pila,PDA):  
     simbolo = w[0:1]
     w=w[1:]
     pila=pila[2:]
-    print("Neuq4:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
     return (w,pila,PDA)
 
-def Neuq3(w,pila,PDA): #estado final 
+def Neuq3(w,pila,PDA):  
     if (pila==""):
         PDA=False
     simbolo = w[0:1]
     w=w[1:]
-    pila=pila[1:] #consume un elemento de la pila
-    print("Neuq3:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
+    pila=pila[1:] 
     return (w,pila,PDA)
 
 
@@ -236,21 +201,18 @@ def Neuq2(w,pila,PDA):
     simbolo = w[0:1]
     w=w[1:]
     pila=pila[1:]
-    print("Neuq2:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
     return (w,pila,PDA)
 
 def Neuq1(w,pila,PDA):
     simbolo = w[0:1]
     w=w[1:]
     pila="$$" + pila
-    print("Neuq1:simbolo ->" + simbolo + " | w ->" +w+"pila ->" +pila)
     return (w,pila,PDA)
 
 def Neuq0(w,pila):
-    simbolo=w[0:1] #saca el primer elemento 
-    w=w[1:] #todo menos el primer elemento 
+    simbolo=w[0:1] 
+    w=w[1:]
     pila="$" + pila
-    print("Neuq0:simbolo ->" + simbolo + "| w->" +w+ "pila ->" + pila)
     return(w,pila)
 
 ################ PDA (2): NEUMONIA ##################
@@ -265,7 +227,7 @@ def pdaNeumonia(w, pila, PDA):
         w,pila,PDA=Neuq2(w,pila,PDA)
 
     while(w[0:1]=="T"):
-        w,pila,PDA=Neuq3(w,pila,PDA) #estado final
+        w,pila,PDA=Neuq3(w,pila,PDA)
 
     while(w[0:1]=="C"):
         w,pila,PDA=Neuq4(w,pila,PDA)
@@ -277,43 +239,39 @@ def pdaNeumonia(w, pila, PDA):
         w,pila,PDA=Neuq6(w,pila,PDA)
     
     while(w[0:1]=="G"):
-        w,pila,PDA=Neuq7(w,pila,PDA) #estado final
+        w,pila,PDA=Neuq7(w,pila,PDA) 
 
     if (not PDA):
-        print("pda: No reconocida [Neumonia]")
+        print("pda: No reconocida [Neumonia]", end=" ")
         return False
     else:
         if(len(w)==0 and len(pila)==0):
-            print("len: Reconocida [Neumonia]")
+            print("len: Reconocida [Neumonia]", end=" ")
             return True
         else:
-            print("len: NO reconocida [Neumonia]")
+            print("len: NO reconocida [Neumonia]", end=" ")
             return False
 
 
 def porcentaje(total,x,y,z):
 
-    print("\n--> Porcentajes: ")
-    print("COVID: " + '{percent:.1%}'.format(percent=x/total))
-    print("Neumonía: " + '{percent:.1%}'.format(percent=y/total))
-    print("RSV: "+ '{percent:.1%}'.format(percent=z/total))
+    print("\n--> Porcentajes: ", end=" ")
+    print("COVID: " + '{percent:.1%}'.format(percent=x/total), end=" ")
+    print("Neumonía: " + '{percent:.1%}'.format(percent=y/total), end=" ")
+    print("RSV: "+ '{percent:.1%}'.format(percent=z/total), end=" ")
 
-#funcion para leer archivo
+#lectura archivo
 
-with open("input.txt",'r') as f: # f is an object file has a method readlines and it returns a list of elems
+with open("input.txt",'r') as f: 
     c, n, r = 0, 0, 0
-    lineas = f.readlines() #when you read file once u read it again and it gives 0 , it wont allow a second reading unless u save it in a var
+    lineas = f.readlines()  
 
     for line in lineas: 
         pila=""
-        w=line.split("\n")[0] #no poner join ni otro split //split divide en una lista but it eleminiates the element u put inside
-        # [0] is the position in list "['AACC','']"
+        w=line.split("\n")[0] 
         print(w)
         PDA=True
 
-        #string = "SAN ISIDRO"
-        #print(string.split(";"))
-        #lineaUno = f.readlines()[0] # [0] is the position in list "['AACC','']"
 
         # Comprobamos cadena en los 3 PDA
         if pdaCovid(w, pila, PDA):
@@ -325,10 +283,10 @@ with open("input.txt",'r') as f: # f is an object file has a method readlines an
         if pdaRsv(w, pila, PDA):
             r += 1
 
-    print("\n--> Resultados finales:")
-    print("C:", c)
-    print("N:", n)
-    print("RSV:", r)
+    print("\n--> Resultados finales: ", end=" ")
+    print("C:", c, end=" ")
+    print("N:", n, end=" ")
+    print("RSV:", r , end=" ")
     print("Ninguno: ", len(lineas) - (c + n + r))
     tot = len(lineas)
     porcentaje(tot, c, n, r)
